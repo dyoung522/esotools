91af04ee19f24b7b237e60ab6592380117c9125b
Donovan Young <dyoung522@gmail.com>
2024-08-15 19:18:11 -0400

    Install and convert to Testify

--- modTools/find_mods_test.go
+++ modTools/find_mods_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
@@ -12,7 +13,7 @@ func init() {
 	modPath := filepath.Join("/", "tmp", "eso", "live", "AddOns", "TestMod")
 
 	AppFs.MkdirAll(modPath, 0755)
-	afero.WriteFile(AppFs, filepath.Join(modPath, "TestMod.txt"), []byte("## Name: TestMNod\n"), 0644)
+	afero.WriteFile(AppFs, filepath.Join(modPath, "TestMod.txt"), []byte("## Title: Test Mod\n"), 0644)
 }
 
 func TestFindMods(t *testing.T) {
@@ -21,22 +22,23 @@ func TestFindMods(t *testing.T) {
 		t.Setenv("ESO_HOME", esoHome)
 
 		mods, err := FindMods()
-		if err != nil {
-			t.Errorf("did not expect error message; got %v", err)
-			return
-		}
 
-		if len(mods) != 1 {
-			t.Error("Did not find any mods")
-		}
-
+		assert.Nil(t, err, "Expected no error")
+		assert.Equal(t, 1, len(mods), "Expected 1 mod to be found")
 	})
 	t.Run("ENV-NOT-SET", func(t *testing.T) {
-		t.Setenv("ESO_HOME", "")
 		_, err := FindMods()
 
-		if err == nil {
-			t.Error("expected error message, not nil")
-		}
+		assert.NotNil(t, err, "Expected an error")
+		assert.Equal(t, "please set the ESO_HOME environment variable and try again", err.Error(), "Expected error message")
+	})
+	t.Run("INVALID-ESO-HOME", func(t *testing.T) {
+		esoHome := filepath.Join("/tmp", "eso", "invalid")
+		t.Setenv("ESO_HOME", esoHome)
+
+		_, err := FindMods()
+
+		assert.NotNil(t, err, "Expected an error")
+		assert.Equal(t, "\"/tmp/eso/invalid\" is not a valid ESO HOME directory", err.Error(), "Expected error message")
 	})
 }
